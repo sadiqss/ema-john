844a980e313a13b6305fc13aab7a85dc930c2828
sadiqss <sadiq.sau48@gmail.com>
2022-03-24 01:51:12 +0000

    ordder summary

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Product.css';
 const Product = (props) => {
-    const { name, img, seller, rstings } = props.product;
+    const { name, img, price, seller, ratings } = props.product;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price:${price}</p>
+                <p><small>Seller:{seller}</small></p>
+                <p><small>Ratings:{ratings}</small></p>
+            </div>
+            <button onClick={() => props.handleAddToCart(props.product)} className='button-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
