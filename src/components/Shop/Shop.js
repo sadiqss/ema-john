cdd88e546cf82a1722a87307904fa6723658d12a
sadiqss <sadiq.sau48@gmail.com>
2022-04-02 14:27:34 +0100

    routing

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import useProducts from '../../hooks/useProducts';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
+    // const [products, setProducts] = useState([]);
+    const [products, setProducts] = useProducts();
     const [cart, setCart] = useState([]);
-    useEffect(() => {
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    }, [])
+
+    // useEffect(() => {
+    //     fetch('products.json')
+    //         .then(res => res.json())
+    //         .then(data => setProducts(data))
+    // }, [])
 
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
